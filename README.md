fa954ef48f48179387a428575851cae70af17584
Ty Todd <swagginty@gmail.com>
2024-09-15 10:24:03 -0400

    integrated with Ty model

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+## Editing the LLM to Santis model
+
+Go to `src/pages/api/message.js` to edit the LLM. At the top you should see this:
+
+```
+const model = "Tytodd/ty2-1";
+```
+
+change model to the name of the model you want to use. In tune studio there is a get code button use that to get the model name.
+
+Next go to `.env` and ` .env.local` and change the `TUNE_API_KEY` to your key.
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- src/pages/api/message.js
+++ src/pages/api/message.js
@@ -1,4 +1,14 @@
-let messages = [];
+let messages = [
+  {
+    role: "system",
+    content:
+      "Your name is Ty you are a 22 year old male who studies computer science at MIT.",
+  },
+];
+require("dotenv").config();
+const api_key = process.env.TUNE_API_KEY;
+const model = "santi/ty2-model-kvcgd2dc";
+// const model = "Tytodd/ty2-1";
 export default async function handler(req, res) {
   // Check if the request method is GET
   if (req.method === "POST") {
@@ -6,35 +16,34 @@ export default async function handler(req, res) {
     const jsonData = req.body;
     console.log(jsonData);
     const response = await getResponse(jsonData.message);
-    console.log(response);
+    console.log("response", response);
     res.status(200).json({ message: response });
   }
 }
 
 async function getResponse(message) {
-  console.log("message", message);
+  console.log("key", api_key);
   messages.push({ role: "user", content: message });
-  console.log("messages", messages);
   const raw_response = await fetch("https://proxy.tune.app/chat/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "sk-tune-BzvdxGoYg06wI3Lv1d0iJUN3RJf9yxnvBu2",
+      Authorization: api_key,
     },
     body: JSON.stringify({
       temperature: 0.8,
       messages: messages,
-      model: "Tytodd/ty2-1",
+      model: model,
       stream: false,
       frequency_penalty: 0,
       max_tokens: 900,
     }),
   });
 
+  console.log("raw_response", raw_response);
+
   const response_json = await raw_response.json();
   const response = response_json["choices"][0]["message"]["content"];
   messages.push({ role: "assistant", content: response });
   return response;
 }
-
-async function getInfo(query) {}
